4217fe527588a3c3dc3621377a8c44d69d0db94c
Teemuro <156086429+Teemuro@users.noreply.github.com>
2025-02-27 17:00:05 +0000

    changed layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@
 import streamlit as st
 import pandas as pd
 
-
-
 df = pd.read_csv('data/titanic_data.csv')
 
 st.title("Titanic Data Analysis")
@@ -12,16 +10,22 @@ st.header("Survivors by gender")
 bar_color = st.color_picker("Pick a color for the bar chart", "#1f77b4")
 
 
-survivors = df[df["Survived"] == 1]
-survivor_counts = survivors["Sex"].value_counts()
+col1, col2 = st.columns(2)
 
-st.bar_chart(survivor_counts, use_container_width=False,width = 250, color=bar_color)
 
-st.header("Embarkation data")
+with col1:
+    survivors = df[df["Survived"] == 1]
+    survivor_counts = survivors["Sex"].value_counts()
+    st.bar_chart(survivor_counts, use_container_width=True, color=bar_color)
 
-embarked_counts = df["Embarked"].value_counts().reset_index()
-embarked_counts.columns = ["Harbour", "Count"]
 
-st.dataframe(embarked_counts)
+with col2:
+    st.header("Embarkation data")
+    
+    embarked_counts = df["Embarked"].value_counts().reset_index()
+    embarked_counts.columns = ["Harbour", "Count"]
+    embarked_counts["Harbour"] = embarked_counts["Harbour"].replace({"S": "Southampton", "C": "Cherbourg", "Q": "Queenstown"})
+    
+    st.dataframe(embarked_counts, use_container_width=True)
 
 
