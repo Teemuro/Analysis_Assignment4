88281d47f7e592750599fc6ed24868e057159090
Teemuro <156086429+Teemuro@users.noreply.github.com>
2025-02-27 16:35:32 +0000

    barchart and dataframe added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
-import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+import pandas as pd
+
+
+
+df = pd.read_csv('data/titanic_data.csv')
+
+st.title("Titanic Data Analysis")
+
+st.header("Survivors by gender")
+bar_color = st.color_picker("Pick a color for the bar chart", "#1f77b4")
+
+
+survivors = df[df["Survived"] == 1]
+survivor_counts = survivors["Sex"].value_counts()
+
+st.bar_chart(survivor_counts, use_container_width=False,width = 250, color=bar_color)
+
+st.header("Embarkation data")
+
+embarked_counts = df["Embarked"].value_counts().reset_index()
+embarked_counts.columns = ["Harbour", "Count"]
+
+st.dataframe(embarked_counts)
+
+
